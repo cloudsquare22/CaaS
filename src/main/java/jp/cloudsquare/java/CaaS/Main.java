f0b85b34257566e77d3a0f04d531e11964b4fdb5
shi-n <shi-n_xyz@cloudsquare.jp>
2021-01-27 17:02:07 +0900

    Add PropertyData.

--- src/main/java/jp/cloudsquare/java/CaaS/Main.java
+++ src/main/java/jp/cloudsquare/java/CaaS/Main.java
@@ -16,6 +16,7 @@ public class Main {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        PropertyData propertyData = new PropertyData("./example.properties");
     }
     
 }
