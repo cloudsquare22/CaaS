5fa743133eaa9551b3072cf9b853c7cb56490ca7
shi-n <shi-n_xyz@cloudsquare.jp>
2021-01-27 17:03:06 +0900

    Add PropertyData.

---
+++ src/main/java/jp/cloudsquare/java/CaaS/PropertyData.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jp.cloudsquare.java.CaaS;
+
+import java.io.BufferedReader;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Map;
+import java.util.Properties;
+
+/**
+ *
+ * @author inaba
+ */
+public class PropertyData {
+    Properties properties = new Properties();
+
+    public PropertyData(String filePath) {
+        try(BufferedReader bufferedReader =  Files.newBufferedReader(Paths.get(filePath), StandardCharsets.UTF_8);) {
+            properties.load(bufferedReader);
+            for(Map.Entry<Object, Object> entry : properties.entrySet()) {
+                Object key = entry.getKey();
+                Object val = entry.getValue();
+                System.out.println(key + ":" + val);
+            }
+        }
+        catch(Exception e) {
+            System.out.println(e);
+        }
+    }
+}
